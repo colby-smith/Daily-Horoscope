9beaabb2dd6766cd634b2031c681558726ead8a8
Colby <colbysmith99@hotmail.com>
2024-03-12 15:43:49 +0000

    update main.py
    Added get starsign function so that users can enter their starsign in order to get their own horoscope

--- main.py
+++ main.py
@@ -12,18 +12,34 @@ SOUP = BeautifulSoup(PAGE.text, "html.parser")
 content = SOUP.find_all(class_="css-1wfsl5s et3p2gv0")
 
 def main():
-    get_date()
-    get_horoscope(SOUP)
+    starsign = get_starsign()
+    get_date(starsign)
+    # get_horoscope(starsign)
     
-def get_date():
+def get_starsign():
+    get_starsign = input("Please enter your starsign to recieve your daily horoscope: ").strip().lower()
+    starsign_list = ["scorpio", "aries", "cancer", "capricorn", "gemini", "libra", "taurus", "virgo", "aquarius", "pisces", "leo", "sagittarius"]
+    if get_starsign in starsign_list:
+        return (get_starsign)
+    else:
+        raise Exception ("Input must be one of the twelve starsigns")
+    
+def get_date(starsign):
     today = date.today()
     formated_date = today.strftime("%B %d")
     display_date = (f"Your daily horoscope for {formated_date}th is:")
     print (f"-------------------------------------------{display_date}-------------------------------------------")
 
 
-def get_horoscope(SOUP):
-    content_elements = SOUP.find_all(class_="css-1nd4gv7 et3p2gv0")
+def get_horoscope(starsign):
+    headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
+    if starsign == "aquarius":
+        url = "https://www.elle.com/horoscopes/daily/a107/aquarius-daily-horoscope/"
+        return (url)
+        
+    page = requests.get(f"https://www.elle.com/horoscopes/daily/a104/{starsign}-daily-horoscope/", headers=headers)
+    soup = BeautifulSoup(page.text, "html.parser")
+    content_elements = soup.find_all(class_="css-1nd4gv7 et3p2gv0")
     for content in content_elements:
         horoscope_text = content.text.strip()
         if "See All Signs" not in horoscope_text:
